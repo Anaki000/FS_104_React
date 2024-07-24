c57d0873daa00a5ab16a087f8176c22c74258dd6
Мохонько Кирило <67262195+Anaki000@users.noreply.github.com>
2024-07-24 14:04:57 +0300

    components

---
+++ src/components/Greetings/Greetings.jsx
@@ -0,0 +1,11 @@
+const Greetings = () => {
+    return (
+        <section>
+          Welcome 
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda iure optio eaque rem culpa officiis maiores praesentium id similique? Omnis laboriosam optio tempora placeat voluptatibus nisi dolorum ea nemo beatae.
+          Et pariatur non aliquam mollitia quo officia facere iste minima cum sunt, quaerat eos recusandae reiciendis dignissimos cupiditate totam nobis maxime. Molestiae nemo consequuntur iusto modi error amet. Asperiores, repellat?</p>
+        </section>
+    )
+}
+
+export default Greetings;
---
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -0,0 +1,12 @@
+const WelcomeSection = () => {
+    return (
+        <section>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus odio delectus iste aperiam dolorum, cupiditate maxime, minima explicabo repellat sapiente, dolor odit cumque voluptates id rem praesentium voluptatem a sint.</p>
+            <button>click</button>
+            <button>click</button>
+            <button>click</button>
+        </section>
+    )
+}
+
+export default WelcomeSection;
